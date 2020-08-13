eec44f3e185de3097fbcd59c8676f8527cb1028c
SVDevelop <verstka.sv@mail.ru>
2020-08-13 10:19:52 +0300

    fifst commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import './normalize.css';
+import './style.css';
+
+function Test() {
+  return React.createElement('button', null, 'node text')
+}
+const Header = ({texts}) => <h1 id="hi">{texts}</h1>
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <Header texts={'text'} />
   </React.StrictMode>,
   document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
